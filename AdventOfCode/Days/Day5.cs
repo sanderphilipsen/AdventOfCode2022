0e302cd9358a721d2928534e6ba6cf2c8388a719
Sander Philipsen <Sander.Philipsen.ext@dematic.com>
2022-12-05 09:43:41 +0100

    Cleanup day5

--- AdventOfCode/Days/Day5.cs
+++ AdventOfCode/Days/Day5.cs
@@ -2,22 +2,20 @@
 {
     internal class Day5 : BaseDay
     {
-
-        private Stack<char>[] stacks = new Stack<char>[9];
-
+        private readonly Stack<char>[] _stacks = new Stack<char>[9];
 
         internal Day5() : base(5, false)
         {
-            InitStacks();
+            InitStack();
         }
         protected override void ExecuteDay(string[] lines, string[]? linesForB = null)
         {
             FirsStar(lines);
-            InitStacks();
+            InitStack();
             SecondStar(lines);
-
         }
-        private void FirsStar(string[] lines)
+
+        private void FirsStar(IEnumerable<string> lines)
         {
             var resultString = "";
             foreach (var line in lines)
@@ -27,19 +25,19 @@
 
                 for (var i = 0; i < numberOfMoves; i++)
                 {
-                    var took = stacks[int.Parse(lineInput[3]) - 1].Pop();
-                    stacks[int.Parse(lineInput[5]) - 1].Push(took);
+                    var took = _stacks[int.Parse(lineInput[3]) - 1].Pop();
+                    _stacks[int.Parse(lineInput[5]) - 1].Push(took);
                 }
             }
-            for (var i = 0; i < stacks.Length; i++)
+            for (var i = 0; i < _stacks.Length; i++)
             {
-                var took = stacks[i].Pop();
+                var took = _stacks[i].Pop();
                 resultString += took;
             }
             Console.WriteLine(resultString);
         }
 
-        private void SecondStar(string[] lines)
+        private void SecondStar(IEnumerable<string> lines)
         {
             var resultString = "";
             foreach (var line in lines)
@@ -47,49 +45,45 @@
                 var lineInput = line.Split(' ');
                 var numberOfMoves = int.Parse(lineInput[1]);
 
-                List<char> toAdd = new List<char>();
+                var toAdd = new List<char>();
+
                 for (var i = 0; i < numberOfMoves; i++)
-                {
-                    var took2 = stacks[int.Parse(lineInput[3]) - 1].Pop();
-                    toAdd.Add(took2);
-                }
+                    toAdd.Add(_stacks[int.Parse(lineInput[3]) - 1].Pop());
+
                 toAdd.Reverse();
+
                 foreach (var add in toAdd)
-                {
-                    stacks[int.Parse(lineInput[5]) - 1].Push(add);
-                }
+                    _stacks[int.Parse(lineInput[5]) - 1].Push(add);
+
                 toAdd.Clear();
             }
-            for (var i = 0; i < stacks.Length; i++)
-            {
-                var took = stacks[i].Pop();
-                resultString += took;
-            }
+
+            for (var i = 0; i < _stacks.Length; i++)
+                resultString += _stacks[i].Pop();
+
             Console.WriteLine(resultString);
+        }
+
+        private void InitStack()
+        {
+            _stacks[0] = FillStack('p', 'f', 'm', 'q', 'w', 'g', 'r', 't'); ;
+            _stacks[1] = FillStack('h', 'f', 'r');
+            _stacks[2] = FillStack('p', 'z', 'r', 'v', 'g', 'h', 's', 'd');
+            _stacks[3] = FillStack('q', 'h', 'p', 'b', 'f', 'w', 'g');
+            _stacks[4] = FillStack('p', 's', 'm', 'j', 'h');
+            _stacks[5] = FillStack('m', 'z', 't', 'h', 's', 'r', 'p', 'l');
+            _stacks[6] = FillStack('p', 't', 'h', 'n', 'm', 'l');
+            _stacks[7] = FillStack('f', 'd', 'q', 'r');
+            _stacks[8] = FillStack('d', 's', 'c', 'n', 'l', 'p', 'h');
         }
 
         private Stack<char> FillStack(params char[] letters)
         {
             var result = new Stack<char>();
             foreach (var letter in letters)
-            {
                 result.Push(letter);
-            }
 
             return result;
-        }
-
-        private void InitStacks()
-        {
-            stacks[0] = FillStack('p', 'f', 'm', 'q', 'w', 'g', 'r', 't'); ;
-            stacks[1] = FillStack('h', 'f', 'r');
-            stacks[2] = FillStack('p', 'z', 'r', 'v', 'g', 'h', 's', 'd');
-            stacks[3] = FillStack('q', 'h', 'p', 'b', 'f', 'w', 'g');
-            stacks[4] = FillStack('p', 's', 'm', 'j', 'h');
-            stacks[5] = FillStack('m', 'z', 't', 'h', 's', 'r', 'p', 'l');
-            stacks[6] = FillStack('p', 't', 'h', 'n', 'm', 'l');
-            stacks[7] = FillStack('f', 'd', 'q', 'r');
-            stacks[8] = FillStack('d', 's', 'c', 'n', 'l', 'p', 'h');
         }
     }
 }
