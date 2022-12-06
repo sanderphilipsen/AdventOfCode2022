7c9ffaccbf5a0a0e3b5b24d7cfc5d96ee6024fe2
Sander Philipsen <Sander.Philipsen.ext@dematic.com>
2022-12-06 08:24:17 +0100

    Day 6 refactoring

--- AdventOfCode/Days/Day6.cs
+++ AdventOfCode/Days/Day6.cs
@@ -17,21 +17,22 @@
         private void ExecuteStar(string[] lines, int numberOfUniques)
         {
             var chars = lines[0].ToCharArray();
-            for (int i = 0; i < chars.Length; i++)
+            for (var i = 0; i < chars.Length; i++)
             {
                 var list = new List<char>();
                 for (int x = 0; x < numberOfUniques; x++)
-                {
                     list.Add(chars[i + x]);
-                }
-                if (list.Distinct().Count() == numberOfUniques)
-                {
-                    if (numberOfUniques == 14)
-                        SecondStarResult = i + numberOfUniques;
-                    if (numberOfUniques == 4)
-                        FirstStarResult = i + numberOfUniques;
-                    break;
-                }
+
+                if (list.Distinct().Count() != numberOfUniques)
+                    continue;
+
+                if (numberOfUniques == 14)
+                    SecondStarResult = i + numberOfUniques;
+                else if (numberOfUniques == 4)
+                    FirstStarResult = i + numberOfUniques;
+
+                break;
+
             }
         }
     }
